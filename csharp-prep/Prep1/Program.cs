18ecad695570f9a83bce69cc3fa5299c7ffbf98c
butros01 <butrosmbuvi@gmail.com>
2023-04-20 21:14:39 +0300

    complete prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string second_name= Console.ReadLine();
         Console.WriteLine($"Your name is {second_name}, {first_name} {second_name}");
     }
