4eb4a5ad251f4b0d01aa5707988f5eb07fce90a8
butros01 <butrosmbuvi@gmail.com>
2023-04-23 20:43:51 +0300

    complete prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,11);
+        int userGuess = -1;
+        while (userGuess != magicNumber)
+        {   
+            Console.Write("What is the magic magicNumber");
+            userGuess = int.Parse(Console.ReadLine());
+            
+            if (magicNumber > userGuess)
+            {
+            Console.WriteLine("Higher");
+            }
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
