094ee418b1d0467d85af78c88bf8497661802146
butros01 <butrosmbuvi@gmail.com>
2023-05-14 00:39:58 +0300

    update journal program

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -8,7 +8,8 @@ public class Prompt
         "What was the best part of my day?", 
         "How did I see the hand of the Lord in my life today?", 
         "What was the strongest emotion I felt today?", 
-        "If I had one thing I could do over today, what would it be?"
+        "If I had one thing I could do over today, what would it be?",
+        "How did I profress towards achieving my set goal for this year"
     };
 
     public string randomPrompt()
