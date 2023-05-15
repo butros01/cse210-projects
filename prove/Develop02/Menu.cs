9c44b55d313595f37b44827ee81bbd9aa538ec01
butros01 <butrosmbuvi@gmail.com>
2023-05-15 17:45:48 +0300

    W05 Learning Activity on Encapsulation

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -5,9 +5,10 @@ public class Menu
     public string _display = "Display";
     public string _save = "Save";
     public string _load = "Load";
+    public string _quit = "Quit"; 
 
     public void Display()
     {
-        Console.WriteLine($"Please select one of the following choices:\n1.{_write}\n2.{_display}\n3.{_save}\n4.{_load}\n ");
+        Console.WriteLine($"Please select one of the following choices:\n1.{_write}\n2.{_display}\n3.{_save}\n4.{_load}\n5.{_quit}\n ");
     }
 }
